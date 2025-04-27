bc6b604e187dde66083e4b3d60b313797139308f
JRCockrum <jaco7466@colorado.edu>
2025-04-26 20:35:09 -0600

    kmeans almost done

--- src/cluster.py
+++ src/cluster.py
@@ -1,19 +1,18 @@
 import numpy as np
 import pandas as pd
+# from kmeans import Kmeans
 
 class Cluster:
     def __init__(self, kmeans):
-        # TODO: I'm not sure about some of these default values
         self._centroid = None
         self._data =  [] # list of indexes
-        self._error = None # TODO might call this somthing else (e.g squared dist)
         self._kmeans = kmeans
 
     def add_row(self, row):
         #TODO add check that row is not in data
         self._data.append(row)
         self._update_centroid()
-        self._update_error()
+
 
     def drop_row(self, row): # row: expected to be a 1D NumPy array
         try:
@@ -43,7 +42,11 @@ class Cluster:
         pass
 
     def get_error(self):
-        return self._error
+        error = 0
+        for row_num in self._data:
+            error += self._kmeans.dist(self._centroid, self._kmeans.lookup_index(row_num).values) ** 2
+        return error
+
     
     def get_data(self):
         return self._data
--- src/kmeans.py
+++ src/kmeans.py
@@ -1,13 +1,14 @@
 from cluster import Cluster
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 class Kmeans:
-    def __init__(self, k: int, data: pd.DataFrame, tolerance, max_iters):
+    def __init__(self, k: int, data: pd.DataFrame, tol=0.05):
         self._data = data
         self._clusters = [Cluster(self) for _ in range(k)]
-        self._tol = tolerance
-        self._max_iters = max_iters
+        self._tol = tol
+        self._error = 0
 
 
     def get_clusters(self):
@@ -20,3 +21,52 @@ class Kmeans:
             raise IndexError(f"Index {index} is out of bounds for the cluster data.") from e
         except Exception as e:
             raise RuntimeError(f"Unexpected error when looking up index {index}: {e}") from e
+        
+    def run_kmeans(self):
+        # TODO init cluster with rand datapoints
+        prev_error = np.inf
+        while True:
+            for row_num in range(len(self._data)):
+                centroids = [clust.get_centroid() for clust in self._clusters]
+                row_vals = self.lookup_index(row_num)
+                closest_centroid = 0
+                closest_dist = self.dist(centroids[0], row_vals)
+
+                for centroid_num in range(len(centroids)):
+                    d = self.dist(centroids[centroid_num], row_vals)
+                    if d < closest_dist:
+                        closest_centroid = centroid_num
+                        closest_dist = d
+
+                for clust in self._clusters:
+                    if clust.has_row(row_num):
+                        clust.drop_row(row_num)
+                self._clusters[closest_centroid].add_row(row_num)
+
+
+            error = self.calculate_error()
+            if abs(prev_error - error) <= self._tol:
+                self.show_clusters()
+                return
+            else:
+                prev_error = error
+
+        
+    def dist(self, x, y):
+        return np.sqrt(sum((x_i-y_j)**2 for x_i, y_j in zip(x, y)))
+
+    def calculate_error(self):
+        error = 0
+        for cluster in self._clusters:
+            error += cluster.get_error()
+        return error
+    
+    def show_clusters(self):
+        # TODO add ability to selext axes + title + labels
+        plt.figure(figsize=(8, 6))
+        for cluster in self._clusters:
+            x = [self.lookup_index(idx).values[0] for idx in cluster.get_data()]
+            y = [self.lookup_index(idx).values[1] for idx in cluster.get_data()]
+            plt.scatter(x, y)
+        plt.show()
+        
