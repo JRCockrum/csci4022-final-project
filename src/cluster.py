c21f36892480b17ce01c07e74a0b8833845c59b6
JRCockrum <jaco7466@colorado.edu>
2025-04-27 16:15:46 -0600

    kmeans should work with categorical data

--- src/cluster.py
+++ src/cluster.py
@@ -4,50 +4,50 @@ import pandas as pd
 
 class Cluster:
     def __init__(self, kmeans):
-        self._centroid = None
+        self._numeric_centroid = None
+        self._cat_centroid = None
         self._data =  [] # list of indexes
         self._kmeans = kmeans
 
     def add_row(self, row):
-        #TODO add check that row is not in data
-        self._data.append(row)
-        self._update_centroid()
+        if not self.has_row(row):
+            self._data.append(row)
+            self._update_centroids()
 
-
-    def drop_row(self, row): # row: expected to be a 1D NumPy array
+    def drop_row(self, row):
         try:
             self._data.remove(row)
-            self._update_centroid()
-            self._update_error()
+            self._update_centroids()
         except ValueError:
             print("Error: unable to remove row because it was not found in the cluster")
             print(f"Row: {row}")
 
-    def _update_centroid(self):
+    def _update_centroids(self):
         if not self._data:
-            self._centroid = None
+            self._numeric_centroid = None
+            self._cat_centroid = None
             return
-        # Get the actual rows from the parent KMeans object
-        rows = [self._kmeans.lookup_index(i).values for i in self._data]
-        self._centroid = np.mean(rows, axis=0)
+        
+        # Update Numeric centroid
+        numeric_rows = [self._kmeans.lookup_numeric(i) for i in self._data]
+        self._numeric_centroid = np.mean(numeric_rows, axis=0)
 
-    def get_centroid(self):
-        return self._centroid
+        # Update categorical centroid
+        cat_rows = [self._kmeans.lookup_cat(i) for i in self._data]
+        self._cat_centroid = set().union(*cat_rows)
+
+    def get_num_centroid(self):
+        return self._numeric_centroid
     
-    def _update_error(self):
-        #TODO
-        if not self._data:
-            self._error = None
-            return
-        pass
-
+    def get_cat_centroid(self):
+        return self._cat_centroid
+    
     def get_error(self):
         error = 0
         for row_num in self._data:
-            error += self._kmeans.dist(self._centroid, self._kmeans.lookup_index(row_num).values) ** 2
+            error += self._kmeans.dist(self._numeric_centroid, self._kmeans.lookup_numeric(row_num), self._cat_centroid, self._kmeans.lookup_cat(row_num)) ** 2
         return error
 
-    
     def get_data(self):
         return self._data
     
--- src/kmeans.py
+++ src/kmeans.py
@@ -5,11 +5,13 @@ import random
 import matplotlib.pyplot as plt
 
 class Kmeans:
-    def __init__(self, k: int, data: pd.DataFrame, tol=0.05):
+    def __init__(self, k: int, data: pd.DataFrame, numeric_cols: list, cat_cols: list, tol=0.05, a=0.995):
         self._data = data
         self._clusters = [Cluster(self) for _ in range(k)]
+        self._numeric_cols = numeric_cols
+        self._cat_cols = cat_cols
         self._tol = tol
-        self._error = 0
+        self._a = a
 
         #Init Clusters with random datapoint
         random_indices = random.sample(range(len(data)), k)
@@ -20,37 +22,48 @@ class Kmeans:
     def get_clusters(self):
         return self._clusters
 
-    def lookup_index(self, index):
+    def lookup_numeric(self, index):
         try:
-            return self._data.iloc[index, :]
+            return self._data.loc[index, self._numeric_cols].values
+        except IndexError as e:
+            raise IndexError(f"Index {index} is out of bounds for the cluster data.") from e
+        except Exception as e:
+            raise RuntimeError(f"Unexpected error when looking up index {index}: {e}") from e
+        
+    def lookup_cat(self, index):
+        try:
+            return self._data.loc[index, self._cat_cols].values
         except IndexError as e:
             raise IndexError(f"Index {index} is out of bounds for the cluster data.") from e
         except Exception as e:
             raise RuntimeError(f"Unexpected error when looking up index {index}: {e}") from e
         
     def run_kmeans(self):
-        # TODO init cluster with rand datapoints
         prev_error = np.inf
         while True:
             for row_num in range(len(self._data)):
-                centroids = [clust.get_centroid() for clust in self._clusters]
-                row_vals = self.lookup_index(row_num)
-                closest_centroid = 0
-                closest_dist = self.dist(centroids[0], row_vals)
+                num_centroids = [clust.get_num_centroid() for clust in self._clusters]
+                cat_centroids = [clust.get_cat_centroid() for clust in self._clusters]
 
-                for centroid_num in range(len(centroids)):
-                    d = self.dist(centroids[centroid_num], row_vals)
+                num_row_vals = self.lookup_numeric(row_num)
+                cat_row_vals = self.lookup_cat(row_num)
+
+                closest_cluster = 0
+                closest_dist = self.dist(num_centroids[0], num_row_vals, cat_centroids[0], cat_row_vals)
+
+                for centroid_num in range(len(self._clusters)):
+                    d = self.dist(num_centroids[centroid_num], num_row_vals, cat_centroids[centroid_num], cat_row_vals)
                     if d < closest_dist:
-                        closest_centroid = centroid_num
+                        closest_cluster = centroid_num
                         closest_dist = d
 
                 for clust in self._clusters:
                     if clust.has_row(row_num):
                         clust.drop_row(row_num)
-                self._clusters[closest_centroid].add_row(row_num)
-
+                self._clusters[closest_cluster].add_row(row_num)
 
             error = self.calculate_error()
+            print(error)
             if abs(prev_error - error) <= self._tol:
                 self.show_clusters()
                 return
@@ -58,8 +71,17 @@ class Kmeans:
                 prev_error = error
 
         
-    def dist(self, x, y):
+    def dist(self, num_x, num_y, cat_x, cat_y):
+        return 100*(1-self._a)*self._euc_dist(num_x,num_y) + 100*self._a*self._jac_dist(cat_x,cat_y)
+    
+    def _euc_dist(self, x, y):
         return np.sqrt(sum((x_i-y_j)**2 for x_i, y_j in zip(x, y)))
+
+    def _jac_dist(self, x, y):
+        x = set(x)
+        y = set(y)
+        sim = len(x & y) / len(x | y)
+        return 1-sim
 
     def calculate_error(self):
         error = 0
@@ -73,8 +95,8 @@ class Kmeans:
         COLORS = ['red', 'blue', 'green', 'orange', 'purple', 'brown', 'pink', 'gray', 'olive', 'cyan']
 
         for i, cluster in enumerate(self._clusters):
-            x = [self.lookup_index(idx).values[0] for idx in cluster.get_data()]
-            y = [self.lookup_index(idx).values[1] for idx in cluster.get_data()]
+            x = [self.lookup_numeric(idx)[0] for idx in cluster.get_data()]
+            y = [self.lookup_numeric(idx)[1] for idx in cluster.get_data()]
             plt.scatter(x, y, color=COLORS[i])
         plt.show()
         
